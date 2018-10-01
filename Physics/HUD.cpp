3542d5ab1b9face555959e7e11af4819b3d88130
Kyle Hartenstein <krh105@pitt.edu>
2018-10-01 11:36:52 -0400

    basic physics code

---
+++ Physics/HUD.cpp
@@ -0,0 +1,31 @@
+#include "include/HUD.h"
+
+void HUD::init_HUD()
+{
+	//load in the image of HUD (HUD.png)
+	
+}
+
+void HUD::lower_oxygen()
+{
+	//replace the rectangle in the oxygen indicator down a set amount
+	
+}
+
+void HUD::raise_oxygen()
+{
+	//replace the rectangle in the oxygen indicator up a set amount
+	
+}
+
+void HUD::lower_temperature()
+{
+	//replace the rectangle in the temperature indicator down a set amount
+	
+}
+
+void HUD::raise_temperature()
+{
+	//replace the rectangle in the remperature indicator down a set amount
+	
+}
---
+++ Physics/HUD.h
@@ -0,0 +1,15 @@
+#ifndef BANDCAMP_HUD_H_
+#define BANDCAMP_HUD_H_
+
+//initialize the HUD
+class HUD
+{
+	public:
+		init_HUD();
+		lower_oxygen();
+		raise_oxygen();
+		lower_temperature();
+		raise_temperature();
+}
+
+#endif  //  BANDCAMP_HUD_H_
