c13587ffc85165a2f60b46bd63d37af319740bd4
Kyle Hartenstein <krh105@pitt.edu>
2018-10-01 16:50:26 -0400

    HUD init

--- src/HUD.cpp
+++ src/HUD.cpp
@@ -1,31 +1,63 @@
 #include "include/HUD.h"
+#include "include/utils.h"
+
+bool init_h;
+std::vector<SDL_Texture*> hud;
+SDL_Renderer* renderer_h;
+SDL_Rect Temp;
+SDL_Rect Oxygen;
+
+HUD::HUD(SDL_Renderer* _renderer)
+{
+    renderer_h = _renderer;
+    init_h = false;
+}
+
+HUD::~HUD()
+{
+    utils::destroyTextureVector(hud);
+}
 
 void HUD::init_HUD()
 {
 	//load in the image of HUD (HUD.png)
-	
+	int imgFlags = IMG_INIT_PNG;
+	int retFlags = IMG_Init(imgFlags);
+	hud.push_back(utils::loadTexture(renderer_h, "Physics/HUD.png"));
+	SDL_Rect Temp = {54, 54, 33, 91};
+	SDL_Rect Oxygen = {109, 54, 33, 91};
+	SDL_SetRenderDrawColor(renderer_h, 0x00, 0xFF, 0xFF, 0xFF);
+	SDL_RenderFillRect(renderer_h, &Temp);
+	SDL_RenderFillRect(renderer_h, &Oxygen);
+	SDL_RenderCopy(renderer_h, hud[0], NULL, NULL);
+	SDL_RenderPresent(renderer_h);
+	init_h = true;
 }
 
 void HUD::lower_oxygen()
 {
 	//replace the rectangle in the oxygen indicator down a set amount
+	if(!init_h) return;
 	
 }
 
-void HUD::raise_oxygen()
+void HUD::raise_oxygen(int resource_value)
 {
 	//replace the rectangle in the oxygen indicator up a set amount
+	if(!init_h) return;
 	
 }
 
 void HUD::lower_temperature()
 {
 	//replace the rectangle in the temperature indicator down a set amount
+	if(!init_h) return;
 	
 }
 
-void HUD::raise_temperature()
+void HUD::raise_temperature(int resource_value)
 {
 	//replace the rectangle in the remperature indicator down a set amount
+	if(!init_h) return;
 	
 }
--- src/include/HUD.h
+++ src/include/HUD.h
@@ -1,15 +1,30 @@
 #ifndef BANDCAMP_HUD_H_
 #define BANDCAMP_HUD_H_
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "utils.h"
+
 //initialize the HUD
 class HUD
 {
 	public:
 		void init_HUD();
 		void lower_oxygen();
-		void raise_oxygen();
+		void raise_oxygen(int resource_value);
 		void lower_temperature();
-		void raise_temperature();
+		void raise_temperature(int resource_value);
+		HUD(SDL_Renderer* _gRenderer);
+		~HUD();
+	private:
+		bool init_h;
+		std::vector<SDL_Texture*> hud;
+		SDL_Renderer* renderer_h;
+		SDL_Rect Temp;
+		SDL_Rect Oxygen;
 };
 
 #endif  //  BANDCAMP_HUD_H_
