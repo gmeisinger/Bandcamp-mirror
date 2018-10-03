f4ba03f83a5d5369380e9a49571cdffbf422b23b
cjb122 <cjb122@pitt.edu>
2018-10-03 02:22:20 -0400

    Fixed issue where player was disappearing

--- src/include/HUD.h
+++ src/include/HUD.h
@@ -14,6 +14,7 @@ class HUD
 	public:
 		void init_HUD();
 		void change_levels(int oxygen_level, int temperature_level);
+		void set_renderer(SDL_Renderer* _renderer);
 		HUD(SDL_Renderer* _gRenderer);
 		~HUD();
 	private:
