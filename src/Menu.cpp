5f1818239e3898b64f64b6486556cfc924194071
Jake Bader <jsb99@pitt.edu>
2018-10-17 09:08:32 -0400

    Skipping over new code so it doesn't break master

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,3 +1,4 @@
+#ifdef BANDCAMP_MENU_H_ //This framework is incomplete, so for the moment it is skipped over for compilation
 #include <SDL.h>
 #include <SDL_image.h>
 
@@ -5,9 +6,15 @@
 #include "include/utils.h"
 #include "include/button.h"
 #include "include/menu.h"
-Button *test_button;
+
+//This is the basis for a repurposable object-oriented menu. The primary idea here is that each menu item (buttons, sliders, etc.)
+//will be added to a data structure in this menu class, which will then manage input and detect when the items have been activated.
+//Development was cut short for the week of 10/17, because of issues that need to be further discussed with the team
+
+
+Button *test_button; //test button for proof of concept
 TTF_Font *font = NULL;
-Menu::Menu(char* label) {
+Menu::Menu(char* label) { //Constructs a test button; in the future, this should be more flexible and adding items will happen via another method.
 	int clicked = 0;
 	SDL_Rect r;
 	r.x = 400;
@@ -22,11 +29,13 @@ void Menu::init() {
 	font = TTF_OpenFont("Arial.ttf", 16);
 }
 
-void Menu::update() {
+void Menu::update() { //Blank update function for future use
 	
 }
 
-void Menu::input() {
+void Menu::input() { //In the future, the input will probably look similar, but instead of checking against only the test_button when clicked it will
+					 //iterate through all the items in the menu and see if any are activated by a mouse click.
+//mouse x and mouse y
 	int mx;
 	int my;
 	if(clicked == 1) {
@@ -42,7 +51,8 @@ void Menu::input() {
 }
 
 
-SDL_Renderer* Menu::draw(SDL_Renderer *renderer, SDL_Surface *screen) {
+SDL_Renderer* Menu::draw(SDL_Renderer *renderer, SDL_Surface *screen) { //As with input, in the future this will render every object in a list rather than just test_button
 	test_button->draw(renderer, font, screen);
 	return renderer;
 }
+#endif //BANDCAMP_MENU_H_
--- src/button.cpp
+++ src/button.cpp
@@ -1,16 +1,26 @@
+#ifdef BANDCAMP_BUTTON_H_
+
 #include <SDL.h>
 #include <SDL_image.h>
 #include <SDL_ttf.h>
 #include "include/button.h"
 
-SDL_Rect rectangle;
-SDL_Rect surface_offset;
-char* label;
-SDL_Color color;
+//Framework for a basic object oriented button. In the future, this may be inheritable so that different types
+//of buttons can be made, or more flexible parameters will be added allowing for different images and effects to be loaded
+
+SDL_Rect rectangle; //area of the button
+SDL_Rect surface_offset; //the x and y of this rectangle determine the button's offset
+char* label; //Text to be displayed by the button
+SDL_Color color; //Current button color
+
+//Default pressed, unpressed, and text colors
 SDL_Color unpressed = {180, 180, 180};
 SDL_Color pressed = {90, 90, 90};
 SDL_Color text_color = {0, 0, 0, 0xFF};
+
+//Text will be rendered to a surface and then blitted to the screen
 SDL_Surface *message = NULL;
+
 int is_pressed = 0;
 
 int label_x;
@@ -21,10 +31,11 @@ Button::Button(char* l, SDL_Rect r) { //Constructs the button. For now, it is a 
 	label = l;
 	
 	rectangle = r;
-	color = {180, 180, 180};
+	color = unpressed;
 	
-	int surface_offset_x = rectangle.x + label_offset;
-	int surface_offset_y = rectangle.y + (rectangle.h / 2);
+	//The text will be vertically centered on the button and slightly offset from the left side.
+	int label_x = rectangle.x + label_offset;
+	int label_y = rectangle.y + (rectangle.h / 2);
 	
 }
 
@@ -42,12 +53,15 @@ void Button::update(const Uint8* keystate) {
 	
 }
 
-SDL_Renderer* Button::draw(SDL_Renderer *renderer,  TTF_Font * font, SDL_Surface *screen) {
+SDL_Renderer* Button::draw(SDL_Renderer *renderer,  TTF_Font * font, SDL_Surface *screen) { //General idea of render; currently, it draws a rectangle, renders text, and blits it to the screen.
 	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 0xFF);
 	SDL_RenderFillRect(renderer, &rectangle);
 	
+	//Issue with this is that it requires the screen to be passed throughout the entire program as a surface object; this will require some restructuring
 	message = TTF_RenderText_Solid(font, label, text_color);
 	SDL_BlitSurface(message, NULL, screen, &surface_offset);
 	
 	return renderer;
 }
+
+#endif
