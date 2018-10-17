6632c50d8a4cea3c0373547b0ac0d13b9bb105f2
jtbudd451 <buddwebsolutions@gmail.com>
2018-10-16 21:45:10 -0400

    added base of proc_gen class

---
+++ src/proc_gen.cpp
@@ -0,0 +1,61 @@
+/* Team Bandcamp
+ * Class function: 
+ * 
+*/
+
+// #include "include/proc_gen.h"
+
+// Generate a new frame for a floor of game for rooms to fit within
+bool genFloor(){
+	bool success = false;
+
+	return success;
+}
+
+// Validates that the floor fits within expected bounds 
+bool validFloor(){
+	bool success = false;
+
+	return success;
+}
+
+// Generate a new room 
+bool genRoom(){
+	bool success = false;
+
+	// Generate walls 
+
+	// Establish collision detection points 
+
+	// Generate door connecting to the rest of the map 
+
+	// call valid room 
+
+	return success;
+}
+
+// Validates that the room wont inhibit game play
+bool validRoom(){
+	bool success = false;
+
+	// check that the player can enter and leave the room 
+
+	// check limits of the room are acceptable for gameplay 
+
+	
+	return success;
+}
+
+// Generate items for gameplay 
+bool genItem(){
+	bool success = false;
+
+	return success;
+}
+
+// Decides items to place 
+bool decideItem(){
+	bool success = false;
+
+	return success;
+}
