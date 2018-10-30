99113ffc6749e81531b90f186c4e0c76d7bc4f1c
Jake Bader <jsb99@pitt.edu>
2018-10-30 19:48:17 -0400

    stashing

---
+++ src/include/button.h
@@ -0,0 +1,36 @@
+#ifndef BANDCAMP_BUTTON_H_
+#define BANDCAMP_BUTTON_H_
+
+#include "object.h"
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+
+
+class Button : public Object
+{
+	private:
+		SDL_Rect rectangle;
+		char* label;
+		Uint8 unpressed;
+		Uint8 pressed;
+		SDL_Color text_color;
+		
+		int is_pressed;
+		int label_x;
+		int label_y;
+		const int label_offset = 15;
+		SDL_Surface * message;
+		SDL_Texture * message_texture;
+
+	public:
+		Button(char * l, SDL_Rect r);
+		void init(SDL_Renderer* renderer);
+		void press();
+		void unpress();
+		void update(std::vector<Object*> objectList, const Uint32 ticks);
+		void input(const Uint8* keystate);
+		SDL_Renderer* draw(SDL_Renderer *renderer);
+};
+
+#endif // BANDCAMP_BUTTON_H_
