a3926c86a9ed0bd8cbd3f567f58183153682e7ed
Kyle Hartenstein <31696257+krh105@users.noreply.github.com>
2018-10-31 09:45:54 -0400

    adv physics base

--- src/physics.cpp
+++ src/physics.cpp
@@ -1,16 +1,26 @@
-/* Team Bandcamp
- * Class function: 
- * 
-*/
-
 #include "include/physics.h"
 
-void Room::init_room()
+//really important to note that this is a physic simulation, NOT real life. If we do REAL physics, whenever a breach is in a pressurized zone, 
+//it rapidly depressurizes and equalizes. That's no fun because we'll instantly die. So we'll have to figure out how to make this interesting
+//but still a physics simulation...
+
+int previous_pressure;
+
+void Room::init_room() //this will always be used for the first room
 {
-	//initialize room values based of procgen
 	//we'll use "percent" values (0-100) to keep things simple
-	int oxygen = 100;
-	int temperature = 100;
+	oxygen = 100;
+	temperature = 100;
+	pressure = 100;
+	previous_pressure = pressure;
+}
+
+void Room::adv_init_room(int o, int t, int p) 	//this will be implemented when multiple rooms are created, taking into account the other room's values
+{
+	oxygen = (o+100)/2;						//for now, takes the previous room's values and this one ("100") and averages them
+	temperature = (t+100)/2;
+	pressure = (p+100)/2;
+	previous_pressure = pressure;
 }
 
 int Room::give_oxygen()
@@ -23,10 +33,44 @@ int Room::give_temperature()
 	return temperature;
 }
 
+void Room::lower_pressure(int num_of_breaches) //depending on how many breaches in the room
+{
+	previous_pressure = pressure;
+	switch(num_of_breaches)				//lets try to figure out a better way for this, but for now this is what we have
+	{
+		case(0):
+			pressure = 100;
+			break;
+		case(1):
+			pressure = 95;
+			break;
+		case(2):
+			pressure = 90;
+			break;
+		case(3):
+			pressure = 80;
+			break;
+		case(4):
+			pressure = 60;
+			break;
+		case(5):
+			pressure = 20;
+			break;
+		case(6):
+			pressure = 0;
+			break;
+	}
+}
+
 void Room::lower_oxygen()
 {
 	//lowers oxygen by a set amount, for now 5?
 	oxygen-=5;
+}
+
+void Room::adv_lower_oxygen() //something porportional with pressure
+{
+	//to be determined
 }
 
 void Room::raise_oxygen(int resource_value)
@@ -39,6 +83,13 @@ void Room::lower_temperature()
 {
 	//lowers temperature by a set amount, for now 5?
 	temperature-=5;
+}
+
+void Room::adv_lower_temperature() //k=T1/P1 T2=k*P2
+{
+	int k = temperature/previous_pressure;
+	int temp = k*pressure;
+	temperature = temp;
 }
 
 void Room::raise_temperature(int resource_value)
