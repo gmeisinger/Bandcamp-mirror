222290d7677f69f932c5ae8bfa4a85a30ef50f89
George Meisinger <gmeisinger@Gmail.com>
2018-11-04 15:33:10 -0500

    Revert "Cjb los"

--- src/include/ooze.h
+++ src/include/ooze.h
@@ -31,9 +31,13 @@ enum oozeState {
 class Ooze : public Object
 {
 private:
+    //This should be removed ASAP
+    SDL_Rect lWall;
+    SDL_Rect rWall;
+    SDL_Rect uWall;
+    Circle cPillar;
+    //
     SDL_Rect rect;
-    //Used to check line of sight
-    SDL_Rect colRect;
 
     int x_vel;
     int y_vel;
@@ -87,10 +91,7 @@ public:
 
     //Movement
     void checkBounds(int max_width, int max_height);
-    bool checkCollision(int curX, int curY, std::vector<std::vector<int>> grid, bool move);
-    bool drawLine(std::vector<std::vector<int>> grid);
-    void moveLine(std::vector<std::vector<int>> grid);;
-
+    void checkCollision(int curX, int curY, std::vector<std::vector<int>> grid);
     
     // Math
     void increaseHostility();
