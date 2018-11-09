901ae0a53ebbbfd281ec303e2ad9199a098c2f78
GraemeR <graeioume@gmail.com>
2018-11-08 19:17:05 -0500

    included global files

---
+++ src/global.cpp
@@ -0,0 +1,10 @@
+/* Team Bandcamp
+ * Class function: Global variables for any class
+ * 
+*/
+
+#include "include/global.h"
+
+int screen_w = SCREEN_WIDTH;
+int screen_h = SCREEN_HEIGHT;
+int tile_s = TILE_SIZE;
---
+++ src/include/global.h
@@ -0,0 +1,12 @@
+#ifndef BANDCAMP_GLOBAL_H_
+#define BANDCAMP_GLOBAL_H_
+
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 600;
+constexpr int TILE_SIZE = 32;
+
+extern int screen_w;
+extern int screen_h;
+extern int tile_s;
+
+#endif  //  BANDCAMP_GLOBAL_H_
