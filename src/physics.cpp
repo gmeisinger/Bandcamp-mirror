5e9600429f4f228160659b734b2bf8073e1d0128
Kyle Hartenstein <31696257+krh105@users.noreply.github.com>
2018-11-29 15:24:49 -0500

    adv almost done

--- src/physics.cpp
+++ src/physics.cpp
@@ -13,18 +13,22 @@ void Room2::init_room() //this will always be used for the first room
 	temperature = 100;
 	pressure = 100;
 	previous_pressure = pressure;
+	bool breached = false;
+	num_breaches = 0;
 }
 
-void Room2::adv_init_room(int o, int t, int p) 	//this will be implemented when multiple rooms are created, taking into account the other room's values
+void Room2::adv_init_room(int o, int t, int p, int o2, int t2, int p2) 	//this will need to be "randomized"
 /* Summary
  * Argument  
  *
 */
 {
-	oxygen = (o+100)/2;						//for now, takes the previous room's values and this one ("100") and averages them
-	temperature = (t+100)/2;
-	pressure = (p+100)/2;
+	oxygen = (o+o2)/2;						//for now, takes the previous room's values and this one ("100") and averages them
+	temperature = (t+t2)/2;
+	pressure = (p+p2)/2;
 	previous_pressure = pressure;
+	bool breached = false;
+	num_breaches = 0;
 }
 
 int Room2::give_oxygen()
@@ -38,36 +42,42 @@ int Room2::give_temperature()
 	return temperature;
 }
 
-void Room2::lower_pressure(int num_of_breaches) //depending on how many breaches in the room
+void Room2::lower_pressure() //depending on how many breaches in the room
 /* Summary
  * Argument  
  *
 */
 {
 	previous_pressure = pressure;
-	switch(num_of_breaches)				//lets try to figure out a better way for this, but for now this is what we have
+	// switch(num_of_breaches)				//lets try to figure out a better way for this, but for now this is what we have
+	// {
+		// case(0):
+			// pressure = 100;
+			// break;
+		// case(1):
+			// pressure = 95;
+			// break;
+		// case(2):
+			// pressure = 90;
+			// break;
+		// case(3):
+			// pressure = 80;
+			// break;
+		// case(4):
+			// pressure = 60;
+			// break;
+		// case(5):
+			// pressure = 20;
+			// break;
+		// case(6):
+			// pressure = 0;
+			// break;
+	// }
+	if(breached)
 	{
-		case(0):
-			pressure = 100;
-			break;
-		case(1):
-			pressure = 95;
-			break;
-		case(2):
-			pressure = 90;
-			break;
-		case(3):
-			pressure = 80;
-			break;
-		case(4):
-			pressure = 60;
-			break;
-		case(5):
-			pressure = 20;
-			break;
-		case(6):
-			pressure = 0;
-			break;
+		int i;
+		for(i=0; i<=num_breaches, i++)
+			pressure-=5;
 	}
 }
 
@@ -87,7 +97,26 @@ void Room2::adv_lower_oxygen() //something porportional with pressure
  *
 */
 {
-	//to be determined
+	//PV=nRT
+	double P = (double)pressure;
+	double V = 8.314472; 			//since we are assuming this can be whatever we want, which we'll set to match the gas constant R (for percentage values)
+	double n; 						//what we are looking for
+	double R = 8.314472 			//this is the Ideal Gas Law's constant
+	double T = (double)temperature;
+	
+	//rearranging formula to n = (PV)/(RT)
+	n = (P*V)/(R*T);
+	
+	//now that we have moles, we need to translate it to a percentage value
+	if(n>=1)
+	{
+		oxygen = 100;
+	}
+	else
+	{
+		oxygen = 100*(int)n;
+	}
+	printf("oxygen level: %d", oxygen);
 }
 
 void Room2::raise_oxygen(int resource_value)
@@ -113,10 +142,51 @@ void Room2::adv_lower_temperature() //k=T1/P1 T2=k*P2
 	int k = temperature/previous_pressure;
 	int temp = k*pressure;
 	temperature = temp;
+	printf("temperature level: %d", temperature);
 }
 
 void Room2::raise_temperature(int resource_value)
 {
 	//raises temperature by the value of the resource that was picked up (set by procgen team)
 	temperature+=resource_value;
+}
+
+void Breach::set_type(int type, Room2 room)
+{
+	room.breached = true;
+	if(type == 1)
+	{
+		isInterior = true;
+	}
+	if(type == 2)
+	{
+		isExterior = true;
+	}
+}
+
+void Breach::check_type()
+{
+	//need to figure out how this is being stored
+	//pretty much will check for adjacent rooms then call Breach::set_type whatever is calculated
+}
+
+int Breach::return_type()
+{
+	if(isInterior)
+	{
+		return 1;
+	}
+	if(isExterior)
+	{
+		return 2;
+	}
+}
+
+void Breach::seal_breach()
+{
+	num_breaches-=1;
+	if(num_breaches==0)
+	{
+		breached = false;
+	}
 }
