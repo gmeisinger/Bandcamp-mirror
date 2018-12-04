9341985167e6ed57ec0f3e57f23ec329304a8e03
Jake Bader <jsb99@pitt.edu>
2018-12-04 14:31:49 -0500

    Better descriptors

--- src/Artifact_Descriptor.cpp
+++ src/Artifact_Descriptor.cpp
@@ -1,13 +1,57 @@
-#include 'include/Artifact_Descriptor.h'
+#include "include/Artifact_Descriptor.h"
+std::vector<std::string> historical_figures;
 
 Artifact_Descriptor::Artifact_Descriptor() {
-	
+	std::vector<std::string> t_vec(10, "");
+	historical_figures = t_vec;
+	int i;
+	for(i = 0; i < 10; i++) {
+		historical_figures[i] = get_ruler();
+	}
 }
 
 std::string Artifact_Descriptor::get_first_descriptor() {
-	std::string s = "An item owned by an unknown ruler. ";
+	std::string s = "An item owned by " + historical_figures[rand() % 10] + ". ";
 	return s;
 }
+
+std::string Artifact_Descriptor::get_name() {
+	std::vector<std::string> syllables {
+		"go", "slof", "rin", "pol", "tir", "melp", "zix", "nil",
+		"grem", "drard", "darg", "oem", "oen", "quet", "woi", "ez",
+		"rop", "trel", "yihl", "ull", "ip", "oer", "puz", "aurt",
+		"seir", "duey", "flune", "gaio", "herz", "jyk", "klao", "leiz",
+		"zrif", "xont", "ximm", "xiaa", "cret", "coel", "veot", "bim", "newl",
+		"mrav", "jhoer", "bual", "xol", "ghel", "awr", "eia", "eri", "it", "imk"
+	};
+	int num_syllables = rand() % 5 + 1;
+	std::string name = "";
+	int i;
+	for(i = 0; i < num_syllables; i++) {
+		name += syllables[rand() % syllables.size()];
+	}
+	return name;
+}
+std::string Artifact_Descriptor::get_ruler() {
+	std::string title;
+	int r = rand() % 5;
+	switch(r) {
+		case 0 : title = "King ";
+			break;
+		case 1 : title = "Queen ";
+			break;
+		case 2 : title = "Tyrant ";
+			break;
+		case 3 : title = "Master ";
+			break;
+		case 4 : title = "Oozelord ";
+			break;
+		default : title = "King ";
+	}
+	std::string full_name = title + get_name();
+	return full_name;
+}
+
 
 std::string Artifact_Descriptor::get_second_descriptor() {
 	int r = rand() % 10;
@@ -39,8 +83,8 @@ std::string Artifact_Descriptor::get_second_descriptor() {
 	return s;
 }
 
-// Usage of this class is pretty simple; create an artifact descriptor and call gen_item_description() to get a string containing
-// the two-part description.
+// Usage of this class is pretty simple; initialize an Artifact_Descriptor and call gen_item_description() to get a string containing
+// the two-part description. 
 std::string Artifact_Descriptor::gen_item_description() {
 	std::string s = get_first_descriptor() + get_second_descriptor();
 	return s;
--- src/include/Artifact_Descriptor.h
+++ src/include/Artifact_Descriptor.h
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <string>
 #include <time.h>
+#include <vector>
 
 class Artifact_Descriptor {
 	private:
+		std::string get_name();
+		std::string get_ruler();
+		std::vector<std::string> historical_figures;
 		std::string get_first_descriptor();
 		std::string get_second_descriptor();
 	public:
 		Artifact_Descriptor();
 		std::string gen_item_description();
-}
+};
