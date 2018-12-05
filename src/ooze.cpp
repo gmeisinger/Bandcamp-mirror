66e16c7ef5db42de8fc314431fd8e0fdbc840604
GraemeR <graeioume@gmail.com>
2018-12-05 00:53:58 -0500

    removed comment& merged

--- src/ooze.cpp
+++ src/ooze.cpp
@@ -701,7 +701,7 @@ bool Ooze::drawLine(std::vector<std::vector<Tile*>> &grid, SDL_Rect* target) {
 //This version of Bresenham's moves the player in as stright a line as possible to 
 //the player
 void Ooze::moveLine(std::vector<std::vector<Tile*>> &grid, SDL_Rect* target) {
-    std::cout << "target check" << target << std::endl;
+//    std::cout << "target check" << target << std::endl;
     if(target == nullptr) return;
     int deltaX = target->x - rect.x;
     int deltaY = target->y - rect.y;
